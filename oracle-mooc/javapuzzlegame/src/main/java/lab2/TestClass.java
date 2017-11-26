2f9110339c089243f388c68dfcc74f87320e6968
K4J4N <kajan.14@cse.mrt.ac.lk>
2017-11-26 12:27:52 +0530

    java-puzzle-game lab2

---
+++ oracle-mooc/javapuzzlegame/src/main/java/lab2/TestClass.java
@@ -0,0 +1,34 @@
+package lab2;/* Copyright © 2017 Oracle and/or its affiliates. All rights reserved. */
+
+public class TestClass {
+    public static void main(String[] args) {
+    
+    //Create new instances
+    SavingsAccount savings1 = new SavingsAccount("Savings Account", "Duke", 100, 0.02);
+
+    
+    
+    
+    
+    
+    //Call methods on instances
+    savings1.printDetails();
+    
+    
+    savings1.deposit(5000);
+    savings1.withdraw(100);
+    savings1.earnInterest();
+    savings1.earnInterest();
+    
+    
+    //Create new instances
+    CheckingAccount checking1 = new CheckingAccount("Checking Account", "Duke", 0);
+    CheckingAccount checking2 = new CheckingAccount("Checking Account", "Carrie", 500000);
+    
+    //Call methods on instances
+    checking1.printDetails();
+    checking2.printDetails();
+    
+    
+    }
+}
